c9caeb03aa40193716033f0325008b88faddf797
wuhu <wuhu@itcast.cn>
2020-02-28 10:39:20 +0800

    $.ajaxSetup()全局处理

--- admin/js/http.js
+++ admin/js/http.js
@@ -1,28 +1,46 @@
-/* 沙箱模式 */
-(function(w){
-    var baseURL = 'http://localhost:8080/api/v1'
-    var BigNew = {
-        baseURL:baseURL,//基地址
-        user_login:      baseURL + '/admin/user/login',//用户登录
-        user_info:       baseURL + '/admin/user/info',//用户信息
-        user_detail:     baseURL + '/admin/user/detail',//用户详情
-        user_edit:       baseURL + '/admin/user/edit',//用户编辑
-        category_list:   baseURL + '/admin/category/list',//文章类别查询
-        category_add:    baseURL + '/admin/category/add',//文章类别新增
-        category_search: baseURL + '/admin/category/search',//文章类别搜索
-        category_edit:   baseURL + '/admin/category/edit',//文章类别编辑
-        category_delete: baseURL + '/admin/category/delete',//文章类别删除
-        article_query:   baseURL + '/admin/article/query',//文章搜索
-        article_publish: baseURL + '/admin/article/publish',//文章发布
-        article_search:  baseURL + '/admin/article/search',//文章信息查询
-        article_edit:    baseURL + '/admin/article/edit',//文章编辑
-        article_delete:  baseURL + '/admin/article/delete',//文章删除
-        comment_list:    baseURL + '/admin/comment/search',//文章评论列表
-        comment_pass:    baseURL + '/admin/comment/pass',//文章评论通过
-        comment_reject:  baseURL + '/admin/comment/reject',//文章评论不通过
-        comment_delete:  baseURL + '/admin/comment/delete',//文章评论删除
-    };
+(function(){
 
-    //暴露接口
-    w.BigNew = BigNew;
+
+    // 需求：在每次发送 ajax 请求前，添加请求头
+    $.ajaxSetup({
+        // 全局添加请求头，全局请求头不会被 $.ajax() 里的 headers 覆盖掉
+        headers:{
+            // 服务器要求添加请求头 Authorization，值为登录成功后保存到本地存储中的 token 字符串
+            Authorization: localStorage.getItem('token')
+        },
+
+        // 发送 ajax 请求前 - 作业：添加请求进度条
+        beforeSend: function(){
+            // alert('ajax请求发送前触发');
+        },
+
+        // 请求成功 - success 一般不写在全局，因为会被 $.ajax() 里面的 success 覆盖掉
+        // success:function(){
+        //     alert('成功');
+        // },
+        
+        // 请求失败，意味服务器拒绝给你数据，造成这个原因往往是用户没有没有正确的 token
+        // 没有正确的 token 就跳到登录页，让用户重新登录并获取 token
+        error: function(){
+            // alert('失败，请重新登录');
+            // location.href = './login.html';
+            $('.modal').modal();
+            $('.modal p').html('数据获取失败，请重新登录');
+        },
+        
+        // 完成 - 不管成功失败都执行
+        complete: function(){
+            // alert('完成');
+        }
+
+    });
+
+
+    // 点击去登录的按钮后跳转到登录页
+    $('.to-login').click(function(){
+        // 设置为登录页的路径
+        location.href = './login.html';
+    });
+
+
 })(window);
